6ecf4085db7b0ba1539a1a18da7a4f2dcc8a89e3
lilclipse <artemoskin2006@gmail.com>
2026-05-14 23:45:59 +0700

    Добавил стили сайта

--- src/Views/BaseTemplate.php
+++ src/Views/BaseTemplate.php
@@ -11,22 +11,32 @@ class BaseTemplate
         <html lang='ru'>
         <head>
             <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <title>{$title}</title>
+
+            <link rel='stylesheet' href='/supermarket/assets/css/style.css'>
         </head>
+
         <body>
+
             <header>
                 <h1>ИС «Супермаркет»</h1>
+
                 <nav>
-                    <a href='/supermarket/'>Главная</a> |
-                    <a href='/supermarket/products'>Каталог</a> |
+                    <a href='/supermarket/'>Главная</a>
+                    <a href='/supermarket/products'>Каталог</a>
                     <a href='/supermarket/about'>О нас</a>
                 </nav>
-                <hr>
             </header>
 
             <main>
                 {$content}
             </main>
+
+            <footer>
+                © 2026 ИС «Супермаркет»
+            </footer>
+
         </body>
         </html>
         ";
